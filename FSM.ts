e28c57e5edd73da207b4175bbf913e024ec6cf19
RaphaelTheStegosaurus <r.a.s.m.13@hotmail.com>
2026-07-13 21:53:12 -0600

    Making a FSM from the game

--- FSM.ts
+++ FSM.ts
@@ -85,5 +85,62 @@ const FSM = {
       exit: (player: any) => {},
     },
   },
-  COMPSOGNATHUS: {},
+  COMPSOGNATHUS: {
+    FOLLOWING_PLAYER: {
+      //Seguirá al Jugador a cierta distancia mientras lo vea de frente y de espalda se acercara mas.
+      name: "Follow the Player",
+      enter: (player: any) => {},
+      update: (player: any) => {},
+      exit: (player: any) => {},
+    },
+    KEEPING_DISTANCE: {
+      //Mantendrá una distancia frente a el jugador cuando este lo vea de frente si esta muy cerca por frente de el este retrocederá.
+      name: "Keep its Distance",
+      enter: (player: any) => {},
+      update: (player: any) => {},
+      exit: (player: any) => {},
+    },
+    CLIMBING_ONTO_PLAYER: {
+      //Si se acerca al jugador por la espalda este pasara a trepar al instante
+      name: "Climb onto the Players Back",
+      enter: (player: any) => {},
+      update: (player: any) => {},
+      exit: (player: any) => {},
+    },
+    RUNNING_AWAY: {
+      //Este correrá cuando el jugador lo ataque y se ocultara en la vegetación
+      name: "Run away",
+      enter: (player: any) => {},
+      update: (player: any) => {},
+      exit: (player: any) => {},
+    },
+    TAKING_DAMAGE: {
+      //Recibirá Daño si el jugador lo ataca a distancia con un proyectil como una piedra.
+      name: "Take Damage",
+      enter: (player: any) => {},
+      update: (player: any) => {},
+      exit: (player: any) => {},
+    },
+    APPEARING: {
+      //Este spawner en cierta zonas para empezar acosar al jugador
+      name: "Appear in Vegetation",
+      enter: (player: any) => {},
+      update: (player: any) => {},
+      exit: (player: any) => {},
+    },
+    FALLING_FROM_PLAYER: {
+      //Este caerá desde la espalda del jugador al suelo para esconderse en la vegetación
+      name: "Fall from the Player",
+      enter: (player: any) => {},
+      update: (player: any) => {},
+      exit: (player: any) => {},
+    },
+    ATTACKING: {
+      //Este al ver al jugador en el suelo pasara a atacarlo encima para irle bajando salud a este.
+      name: "Attack the Player on the ground",
+      enter: (player: any) => {},
+      update: (player: any) => {},
+      exit: (player: any) => {},
+    },
+  },
 };
